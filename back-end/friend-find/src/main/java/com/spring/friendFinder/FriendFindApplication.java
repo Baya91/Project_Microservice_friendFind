a278643bdaeb5aaa4beabb1d9d76ea148c23dae7
baya91 <bayassin91@gmail.com>
2023-07-19 13:33:22 +0200

    create project

---
+++ back-end/friend-find/src/main/java/com/spring/friendFinder/FriendFindApplication.java
@@ -0,0 +1,13 @@
+package com.spring.friendFinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendFindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendFindApplication.class, args);
+	}
+
+}
---
+++ back-end/friend-find/src/test/java/com/spring/friendFinder/FriendFindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.friendFinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendFindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
